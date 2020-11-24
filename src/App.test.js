98ac08ee92562e27fbdcd342e51f7bb0ee3451bb
Jeremy Langevin <langevinj832@gmail.com>
2020-11-24 12:59:01 -0500

    Tests written for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('it renders without crashing', function() {
+    const { getByText } = render(<App />)
+
+    const title = getByText("Welcome to the color factory!")
+    expect(title).toBeInTheDocument();
 });
+
+it('matches snapshot', function() {
+  const { asFragment } = render(<App />)
+  expect(asFragment).toMatchSnapshot()
+})
