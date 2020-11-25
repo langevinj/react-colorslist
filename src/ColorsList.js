cb92ac936b94ea16893573c0c5ea908565504984
Jeremy Langevin <langevinj832@gmail.com>
2020-11-25 07:36:43 -0500

    Styling on ColorsList

--- src/ColorsList.js
+++ src/ColorsList.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import ColorForm from './ColorForm'
+import './ColorsList.css'
+
 
 function ColorsList({colors}){
     return (
@@ -9,9 +10,10 @@ function ColorsList({colors}){
                 <h3>Welcome to the color factory!</h3>
                 <h2><Link to="/colors/new">Add a color</Link></h2>
             </div>
+            <br></br>
             <div className="ColorsList">
                 <p>Please select a color.</p>
-                {colors === [] ? null : (<ul>{colors.map(color => <li key={color.name} data-testid="color-link"><Link to={`/colors/${color.name}`}>{color.name}</Link></li>)}</ul>)}
+                {colors === [] ? null : (<ul>{colors.map(color => <li key={color.name} data-testid="color-link"><Link to={`/colors/${color.name}`} className="color-link">{color.name}</Link></li>)}</ul>)}
             </div>
         </div>
     )
