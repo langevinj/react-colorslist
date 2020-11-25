f950e93078b73aecb4acea012ca3a24001dc2a2f
Jeremy Langevin <langevinj832@gmail.com>
2020-11-25 07:14:46 -0500

    using local storage for colors

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom"
 import ColorsList  from './ColorsList'
 import Color from './Color'
 import ColorForm from './ColorForm'
+import useLocalStorage from './hooks'
 
-function Routes({colors}) {
-    const [allColors, setColors] = useState(colors)
+function Routes() {
+    const [localColors, setLocalColors] = useLocalStorage('color')
+    
 
     const addColor = (colorObj) => {
-        setColors(oldColors => (
+        setLocalColors(oldColors => (
             [colorObj, ...oldColors]
         ));
     }
@@ -17,19 +19,12 @@ function Routes({colors}) {
         <BrowserRouter>
         <Switch>
             <Route exact path="/colors/new"><ColorForm addColor={addColor}/></Route>
-            <Route exact path="/colors/:color"><Color colors={allColors}/></Route>
-            <Route exact path="/colors"><ColorsList colors={allColors}/></Route>
+            <Route exact path="/colors/:color"><Color colors={localColors}/></Route>
+            <Route exact path="/colors"><ColorsList colors={localColors}/></Route>
             <Redirect to="/colors"></Redirect>
         </Switch>
         </BrowserRouter>
     )
-}
-
-Routes.defaultProps = {
-    colors: [
-        { name: "red", code: "#fc0f03" },
-        { name: "blue", code: "#5203fc" }
-    ]
 }
 
 export default Routes;
---
+++ src/hooks.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react' 
+
+//utilize localStorage, returns the localcolors and a function to set them
+function useLocalStorage(key, initialValue=[]){
+    if (localStorage.getItem(key)) {
+        initialValue = JSON.parse(localStorage.getItem(key))
+    }
+    const [value, setValue] = useState(initialValue);
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [value, key]);
+
+    return [value, setValue]
+}
+
+export default useLocalStorage;
